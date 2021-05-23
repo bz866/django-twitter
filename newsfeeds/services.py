d6fb61b60b1c5267dc46e37462b18db2c7dfbc39
bz866 <bz866@nyu.edu>
2021-05-23 15:53:39 -0400

    newsfeed fanout to followers

---
+++ newsfeeds/services.py
@@ -0,0 +1,18 @@
+from friendships.services import FriendshipService
+from newsfeeds.models import NewsFeed
+
+
+class NewsFeedService():
+
+    def fanout_to_followers(self, tweet):
+        # get all followers of the poster
+        followers = FriendshipService.get_followers(tweet.user)
+        # create newsfeed for all followers
+        newsfeeds = [
+            NewsFeed(user=follower, tweet=tweet)
+            for follower in followers
+        ]
+        # the poster can see own posted tweet
+        newsfeeds.append(NewsFeed(user=tweet.user, tweet=tweet))
+        NewsFeed.objects.bulk_create(newsfeeds)
+
